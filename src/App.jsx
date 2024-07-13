bc38a26ee40707b0b7f4b36da78eb6901e0f145a
CalebBravo <calebphone72@gmail.com>
2024-07-13 12:51:03 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <div className='container'>
+
+        <div className='loader'>
+
+          <div class="gearbox">
+            <div class="overlay"></div>
+            <div class="gear one">
+              <div class="gear-inner">
+                <div class="bar"></div>
+                <div class="bar"></div>
+                <div class="bar"></div>
+              </div>
+            </div>
+            <div class="gear two">
+              <div class="gear-inner">
+                <div class="bar"></div>
+                <div class="bar"></div>
+                <div class="bar"></div>
+              </div>
+            </div>
+            <div class="gear three">
+              <div class="gear-inner">
+                <div class="bar"></div>
+                <div class="bar"></div>
+                <div class="bar"></div>
+              </div>
+            </div>
+            <div class="gear four large">
+              <div class="gear-inner">
+                <div class="bar"></div>
+                <div class="bar"></div>
+                <div class="bar"></div>
+                <div class="bar"></div>
+                <div class="bar"></div>
+                <div class="bar"></div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <div className='text'>
+
+          <h1>Sorry, we're working here</h1>
+
+          <p>Come back later or check my Github, there you will find more projects.</p>
+
+        </div>
+
+      </div>
+    </>
+  )
+};
+
+export default App;
